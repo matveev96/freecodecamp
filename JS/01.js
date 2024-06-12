6a61da95050ad169a5766c0fe43cff4d2f6cf41f
matveev96 <sasha.matveev1996@icloud.com>
2024-06-12 21:41:24 +0300

    update .gitignore, 01.js

--- JS/01.js
+++ JS/01.js
@@ -7,3 +7,10 @@ secondCharacter = character;
 
 let count = 8;
 console.log(count + 1);
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows[2] = 10;
+console.log(rows);
