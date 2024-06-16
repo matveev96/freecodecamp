ff8659495d2e9b8b937fe6a273b24d8aadcf63bf
matveev96 <sasha.matveev1996@icloud.com>
2024-06-16 23:31:25 +0300

    update info

--- JS/01.js
+++ JS/01.js
@@ -26,3 +26,26 @@ console.log(cities);
 let rows = ["Naomi", "Quincy", "CamperChan"];
 rows.push("freeCodeCamp");
 console.log(rows);
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+const call = padRow();
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
