adbd4d0e340a46d5f951e356681d40fdd79815b0
matveev96 <sasha.matveev1996@icloud.com>
2024-06-20 22:14:36 +0300

    add some new rows

--- JS/01.js
+++ JS/01.js
@@ -49,3 +49,7 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
